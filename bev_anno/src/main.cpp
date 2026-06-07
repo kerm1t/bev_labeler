339dbd6f74370cf5764c01a0de5934453c5bb60f
kerm1t <wolfgang.schulz@gmail.com>
2026-06-07 11:49:21 +0200

    better comments

--- bev_anno/src/main.cpp
+++ bev_anno/src/main.cpp
@@ -13,24 +13,20 @@
 #include "include/pointcloud/pointcloud_io.hpp"
 
 #include <chrono>
-float algo_runtime_ms; // 2do: encapsulate in frs.algo
+float algo_runtime_ms; // 2do: walltime?, overall algo? make sure to not repeat the same as in "algowrap.hpp" here
 
 // lloft objects initialized with an instance of pointcloud
 lloft::pointcloud p;
 lloft::pointcloud_synth psynth(p);
 lloft::pointcloud_io pio(p);
 
-// <algo_headers>
-
 #include <mutex>
 std::recursive_mutex m_SubscriberMutex; // ecal callback thread
 bool bnewframe = false;
 
-// 2do: above variables un-global and make available to below includes
-
 #include "init.hpp"
 #include "user.hpp"
-#include "app_algowrp.hpp" // before eCAL
+#include "app_algowrp.hpp"
 #include "app_ecal.hpp"
 
 int main(int argc, char** argv)
@@ -39,9 +35,9 @@ int main(int argc, char** argv)
 
   app_ecal::init(argc, argv);
 
-  // load labels from .json file
-  int i_toggle = 1;
-  std::string filename = "C:/GIT/BEV_labeler/labels.json";
+  // (1) load labels from .json file
+  std::string filename;
+  filename = argc > 1 ? argv[1] : "C:/GIT/BEV_labeler/labels.json";
   std::cout << "main.cpp: loading " << filename << std::endl;
   algo::annotations = algo::load_labels(filename);
   algo::print_labels(algo::annotations);
@@ -53,15 +49,15 @@ int main(int argc, char** argv)
 // unschoen    if (bnewframe)
     {
       auto t1 = std::chrono::high_resolution_clock::now();
-// <algo_run>
+      // (2) Run the algo,
+      //     here: compute z for the given (x,y) using the subscribed ground model
       algo::run_with_Mutex();
       auto t2 = std::chrono::high_resolution_clock::now();
       std::chrono::duration<double, std::milli> ms_double = t2 - t1;
       std::cout << "run_frs " << ms_double.count() << "ms\n";
       algo_runtime_ms = ms_double.count();
-
-      lloft::nvertices = p.numpoints; // so here it works, but not in the eCal thread, hmmm
       
+      // (3) publish the results
       app_ecal::publish_fox_poly(app_ecal::publisher_poly);
 
       bnewframe = false;
